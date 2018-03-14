8e1f3b4d83db832113a85f952c843bc135ee9002
alphamyd <alphamyd@gmail.com>
2018-03-14 21:37:53 +0200

    changes for 'k+' unread count

--- webview.js
+++ webview.js
@@ -1,8 +1,13 @@
 module.exports = Franz => {
 
   getMessages = () => {
-    const news = document.querySelectorAll("#feedlyTabs .simpleUnreadCount")[0];
-    let counter = parseInt(news.innerText);
+    const newsDOM = document.querySelectorAll("#feedlyTabs .simpleUnreadCount")[0].innerHTML;
+    let counter = parseInt(newsDOM);
+    
+    if (newsDOM.indexOf('k') !== -1 || newsDOM.indexOf('+') !== -1) {
+      counter = newsDOM.substring(0, newsDOM.indexOf('k')) + '000';
+    }
+
     Franz.setBadge(counter);
   };
 
