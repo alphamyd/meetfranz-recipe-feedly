6b3ab527c6039d64b2ae1b2dd8a993c962484b82
David Bensoussan <david@bensoussan.xyz>
2019-04-14 18:02:20 +0200

    fix: change k to K in webview.js
    Probably 'k' was used in the past but now it's used with a capital letter

--- webview.js
+++ webview.js
@@ -4,8 +4,8 @@ module.exports = Franz => {
     const newsDOM = document.querySelectorAll("#feedlyTabs .simpleUnreadCount")[0].innerHTML;
     let counter = parseInt(newsDOM);
     
-    if (newsDOM.indexOf('k') !== -1 || newsDOM.indexOf('+') !== -1) {
-      counter = newsDOM.substring(0, newsDOM.indexOf('k')) + '000';
+    if (newsDOM.indexOf('K') !== -1 || newsDOM.indexOf('+') !== -1) {
+      counter = newsDOM.substring(0, newsDOM.indexOf('K')) + '000';
     }
 
     Franz.setBadge(counter);
